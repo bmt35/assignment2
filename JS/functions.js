e9bcc895a150a2633671893113604237100bdf38
Brandon Torchia <bmt35@pitt.edu>
2017-02-15 11:30:32 -0500

    style changes

--- JS/functions.js
+++ JS/functions.js
@@ -1,31 +1,29 @@
 function myFunction() {
-	var firstname=document.getElementById("t1");
-	var lastname=document.getElementById("t2");
-	var radio1=document.getElementById("r0");
-	var radio2=document.getElementById("r1");
-	var radio3=document.getElementById("r2");
-	var output=document.getElementById("output");
-	var x=isRadioChecked();
-	if(firstname.value=='') {
-		output.innerHTML=firstname.name+" is required";
-	} else if(lastname.value=="") {
-		output.innerHTML=lastname.name+" is required";
-	} else  if(x!=true) {
-	 	output.innerHTML=radio1.name+" is required";
-	} else {
-		output.innerHTML="Thank you for successfully submitting the form";
-		document.getElementById('myform').style.display="none";
-	}
-
-	
+  var firstname=document.getElementById("t1");
+  var lastname=document.getElementById("t2");
+  var radio1=document.getElementById("r0");
+  var radio2=document.getElementById("r1");
+  var radio3=document.getElementById("r2");
+  var output=document.getElementById("output");
+  var x=isRadioChecked();
+  if(firstname.value=='') {
+    output.innerHTML=firstname.name+" is required";
+  } else if(lastname.value=="") {
+	output.innerHTML=lastname.name+" is required";
+  } else  if(x!=true) {
+	output.innerHTML=radio1.name+" is required";
+  } else {
+	output.innerHTML="Thank you for successfully submitting the form";
+	document.getElementById('myform').style.display="none";
+  }	
 }
 
 function isRadioChecked() {
-	var radioButtons=document.getElementsByName("gender");
-	for(var x=0; x<radioButtons.length; x++) {
-		if(radioButtons[x].checked){
-			return true;
-		}
+  var radioButtons=document.getElementsByName("gender");
+  for(var x=0; x<radioButtons.length; x++) {
+    if(radioButtons[x].checked) {
+	  return true;
 	}
+  }
 	return false;
 }
